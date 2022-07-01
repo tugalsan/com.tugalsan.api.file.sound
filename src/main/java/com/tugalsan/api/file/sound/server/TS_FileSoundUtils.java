2d1d6d876365c1d12dacb102e69fba75259a4fe4
tugalsan karabacak <tugalsan@gmail.com>
2022-07-01 09:54:31 +0300

    usage of TGS_UnSafe class

--- src/main/java/com/tugalsan/api/file/sound/server/TS_FileSoundUtils.java
+++ src/main/java/com/tugalsan/api/file/sound/server/TS_FileSoundUtils.java
@@ -4,18 +4,19 @@ import java.awt.Toolkit;
 import java.nio.file.Path;
 import javax.sound.sampled.AudioSystem;
 import com.tugalsan.api.thread.server.*;
+import com.tugalsan.api.unsafe.client.*;
 
 public class TS_FileSoundUtils {
 
     public static void playSound(Path soundFile) {
         TS_ThreadRunUtils.once(() -> {
-            try ( var inputStream = AudioSystem.getAudioInputStream(soundFile.toFile());) {
-                var clip = AudioSystem.getClip();
-                clip.open(inputStream);
-                clip.start();
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
+            TGS_UnSafe.execute(() -> {
+                try ( var inputStream = AudioSystem.getAudioInputStream(soundFile.toFile());) {
+                    var clip = AudioSystem.getClip();
+                    clip.open(inputStream);
+                    clip.start();
+                }
+            });
         });
     }
 
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.tugalsan.api.file.sound {
     requires java.desktop;
     requires gwt.user;
     requires com.tugalsan.api.log;
+    requires com.tugalsan.api.unsafe;
     requires com.tugalsan.api.executable;
     requires com.tugalsan.api.thread;
     exports com.tugalsan.api.file.sound.client;
