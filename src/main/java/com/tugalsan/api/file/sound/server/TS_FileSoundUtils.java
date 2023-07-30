d56984bc9fe11cd937c22793ce1159d1f4c3aaca
tugalsan karabacak <tugalsan@gmail.com>
2023-07-30 07:44:59 +0300

    .

--- src/main/java/com/tugalsan/api/file/sound/server/TS_FileSoundUtils.java
+++ src/main/java/com/tugalsan/api/file/sound/server/TS_FileSoundUtils.java
@@ -1,15 +1,15 @@
 package com.tugalsan.api.file.sound.server;
 
+import com.tugalsan.api.thread.server.async.TS_ThreadAsync;
 import java.awt.Toolkit;
 import java.nio.file.Path;
 import javax.sound.sampled.AudioSystem;
-import com.tugalsan.api.thread.server.*;
 import com.tugalsan.api.unsafe.client.*;
 
 public class TS_FileSoundUtils {
 
     public static void playSound(Path soundFile) {
-        TS_ThreadRun.now(() -> {
+        TS_ThreadAsync.now(() -> {
             TGS_UnSafe.run(() -> {
                 try ( var inputStream = AudioSystem.getAudioInputStream(soundFile.toFile());) {
                     var clip = AudioSystem.getClip();
