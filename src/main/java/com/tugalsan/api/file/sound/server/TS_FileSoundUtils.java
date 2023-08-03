431ff3142c7c2086e5e3deb85698dea59345343b
tugalsan karabacak <tugalsan@gmail.com>
2023-08-03 10:38:21 +0300

    .

--- src/main/java/com/tugalsan/api/file/sound/server/TS_FileSoundUtils.java
+++ src/main/java/com/tugalsan/api/file/sound/server/TS_FileSoundUtils.java
@@ -1,23 +1,33 @@
 package com.tugalsan.api.file.sound.server;
 
-import com.tugalsan.api.thread.server.struct.async.TS_ThreadAsync;
+import com.tugalsan.api.thread.server.TS_ThreadKillTrigger;
+import com.tugalsan.api.thread.server.struct.TS_ThreadStructBuilder;
+import com.tugalsan.api.unsafe.client.TGS_UnSafe;
 import java.awt.Toolkit;
 import java.nio.file.Path;
 import javax.sound.sampled.AudioSystem;
-import com.tugalsan.api.unsafe.client.*;
+import javax.sound.sampled.Clip;
 
 public class TS_FileSoundUtils {
 
-    public static void playSound(Path soundFile) {
-        TS_ThreadAsync.now(() -> {
-            TGS_UnSafe.run(() -> {
-                try ( var inputStream = AudioSystem.getAudioInputStream(soundFile.toFile());) {
-                    var clip = AudioSystem.getClip();
-                    clip.open(inputStream);
-                    clip.start();
-                }
-            });
-        });
+    public static void playSound(TS_ThreadKillTrigger killTrigger, Path soundFile) {
+        TS_ThreadStructBuilder.of()//I KNOW
+                .init(() -> {
+                    return TGS_UnSafe.call(() -> {
+                        try (var inputStream = AudioSystem.getAudioInputStream(soundFile.toFile());) {
+                            var clip = AudioSystem.getClip();
+                            clip.open(inputStream);
+                            clip.start();
+                            return clip;
+                        }
+                    });
+                })
+                .main((kt2, clip) -> {
+                    //I NEED THIS EMPTY RUN
+                })
+                .fin(clip -> ((Clip) clip).stop())
+                .cycle_mainValidation(clip -> clip != null && ((Clip) clip).isRunning() && killTrigger.hasNotTriggered())
+                .asyncRun();
     }
 
     public static void beep() {
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,8 @@ module com.tugalsan.api.file.sound {
     requires java.desktop;
     requires gwt.user;
     requires com.tugalsan.api.log;
+    requires com.tugalsan.api.callable;
+    requires com.tugalsan.api.validator;
     requires com.tugalsan.api.unsafe;
     requires com.tugalsan.api.runnable;
     requires com.tugalsan.api.thread;
