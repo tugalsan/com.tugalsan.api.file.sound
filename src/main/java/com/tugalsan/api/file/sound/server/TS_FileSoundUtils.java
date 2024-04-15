a9b6bb8c420199a3fd518687fa7b9c3bbd8b89db
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-04-15 10:52:20 +0300

    .

--- src/main/java/com/tugalsan/api/file/sound/server/TS_FileSoundUtils.java
+++ src/main/java/com/tugalsan/api/file/sound/server/TS_FileSoundUtils.java
@@ -1,32 +1,50 @@
 package com.tugalsan.api.file.sound.server;
 
+import com.tugalsan.api.runnable.client.TGS_RunnableType1;
 import com.tugalsan.api.thread.server.sync.TS_ThreadSyncTrigger;
 import com.tugalsan.api.thread.server.async.TS_ThreadAsyncBuilder;
-import com.tugalsan.api.unsafe.client.TGS_UnSafe;
+import com.tugalsan.api.union.client.TGS_UnionExcuse;
 import java.awt.Toolkit;
+import java.io.IOException;
 import java.nio.file.Path;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 
 public class TS_FileSoundUtils {
 
-    public static void playSound(TS_ThreadSyncTrigger killTrigger, Path soundFile) {
+    public static void playSound(TS_ThreadSyncTrigger killTrigger, Path soundFile, TGS_RunnableType1<Throwable> onError) {
         TS_ThreadAsyncBuilder.of()//I KNOW
                 .init(() -> {
-                    return TGS_UnSafe.call(() -> {
-                        try (var inputStream = AudioSystem.getAudioInputStream(soundFile.toFile());) {
-                            var clip = AudioSystem.getClip();
-                            clip.open(inputStream);
-                            clip.start();
-                            return clip;
-                        }
-                    });
+                    try (var inputStream = AudioSystem.getAudioInputStream(soundFile.toFile());) {
+                        var clip = AudioSystem.getClip();
+                        clip.open(inputStream);
+                        clip.start();
+                        return TGS_UnionExcuse.of(clip);
+                    } catch (UnsupportedAudioFileException | IOException | LineUnavailableException ex) {
+                        return TGS_UnionExcuse.ofExcuse(ex);
+                    }
                 })
-                .main((kt2, clip) -> {
-                    //I NEED THIS EMPTY RUN
+                .main((kt2, u_clip) -> {
                 })
-                .fin(clip -> ((Clip) clip).stop())
-                .cycle_mainValidation(clip -> clip != null && ((Clip) clip).isRunning() && killTrigger.hasNotTriggered())
+                .fin(u_clip -> {
+                    var u = ((TGS_UnionExcuse<Clip>) u_clip);
+                    if (u.isPresent()) {
+                        u.value().stop();
+                    }
+                })
+                .cycle_mainValidation(u_clip -> {
+                    if (killTrigger.hasTriggered()) {
+                        return false;
+                    }
+                    var u = ((TGS_UnionExcuse<Clip>) u_clip);
+                    if (u.isExcuse()) {
+                        onError.run(u.excuse());
+                        return false;
+                    }
+                    return u.value().isRunning();
+                })
                 .asyncRun();
     }
 
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,7 +4,7 @@ module com.tugalsan.api.file.sound {
     requires com.tugalsan.api.log;
     requires com.tugalsan.api.callable;
     requires com.tugalsan.api.validator;
-    requires com.tugalsan.api.unsafe;
+    requires com.tugalsan.api.union;
     requires com.tugalsan.api.runnable;
     requires com.tugalsan.api.thread;
     exports com.tugalsan.api.file.sound.client;
