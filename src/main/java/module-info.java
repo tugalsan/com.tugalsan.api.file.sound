af6dcdf50432f8fb932e347fe44b59f40c5506cc
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-06-27 12:03:30 +0300

    .

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,7 @@ module com.tugalsan.api.file.sound {
     requires java.desktop;
     requires gwt.user;
     requires com.tugalsan.api.log;
-    requires com.tugalsan.api.callable;
+    requires com.tugalsan.api.function;
     
     requires com.tugalsan.api.unsafe;
     requires com.tugalsan.api.thread;
