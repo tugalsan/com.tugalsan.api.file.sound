13b695951bbd9fcc01fc49c75f76a6caa915fc61
tugalsan karabacak <tugalsan@users.noreply.github.com>
2025-03-24 09:17:05 +0300

    .

--- src/main/java/com/tugalsan/api/file/sound/server/TS_FileSoundUtils.java
+++ src/main/java/com/tugalsan/api/file/sound/server/TS_FileSoundUtils.java
@@ -2,7 +2,7 @@ package com.tugalsan.api.file.sound.server;
 
 import com.tugalsan.api.thread.server.sync.TS_ThreadSyncTrigger;
 import com.tugalsan.api.thread.server.async.builder.TS_ThreadAsyncBuilder;
-import com.tugalsan.api.function.client.maythrow.checkedexceptions.TGS_FuncMTCEUtils;
+import com.tugalsan.api.function.client.maythrowexceptions.checked.TGS_FuncMTCUtils;
 import com.tugalsan.api.log.server.TS_Log;
 import java.awt.Toolkit;
 import java.nio.file.Path;
@@ -21,7 +21,7 @@ public class TS_FileSoundUtils {
         TS_ThreadAsyncBuilder.<Clip>of(killTrigger.newChild(d.className).newChild("playSound"))
                 .name(name)
                 .init(() -> {
-                    return TGS_FuncMTCEUtils.call(() -> {
+                    return TGS_FuncMTCUtils.call(() -> {
                         try (var inputStream = AudioSystem.getAudioInputStream(soundFile.toFile());) {
                             var clip = AudioSystem.getClip();
                             clip.open(inputStream);
